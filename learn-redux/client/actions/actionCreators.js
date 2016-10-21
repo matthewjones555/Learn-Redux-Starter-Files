34a91e4a37557a054f94bf9c425b4707f5d73172
Matthew Jones <matthew.jones@slimmingworld.com>
2016-10-21 16:26:17 +0100

    Created actions.

--- learn-redux/client/actions/actionCreators.js
+++ learn-redux/client/actions/actionCreators.js
@@ -1,3 +1,31 @@
 /**
  * Created by matthew.jones on 10/21/2016.
  */
+
+
+//increment likes
+function increment(index) {
+    return {
+        type: 'INCREMENT_LIKES',
+        index,
+    };
+}
+
+//add coment
+function addComment(postId, author, comment) {
+    return {
+        type: 'ADD_COMMENT',
+        postId,
+        author,
+        comment,
+    };
+}
+
+//remove comment
+function removeComment(postId, i) {
+    return {
+        type: 'REMOVE_COMMENT',
+        postId,
+        i,
+    };
+}
