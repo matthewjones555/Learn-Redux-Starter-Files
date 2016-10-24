71e4a0c34634041964863583da2e306387130d45
Matthew Jones <matthew.jones@slimmingworld.com>
2016-10-24 11:57:43 +0100

    Hooked up add comment form submit to invoke the addComment action.

--- learn-redux/client/components/Comments.js
+++ learn-redux/client/components/Comments.js
@@ -16,11 +16,21 @@ class Comments extends React.Component {
         );
     }
 
+    handleSubmit(event) {
+        event.preventDefault();
+
+        const postId = this.props.params.postId;
+        const author = this.refs.author.value;
+        const comment = this.refs.comment.value;
+
+        this.props.addComment(postId, author, comment);
+    }
+
     render() {
         return (
             <div className="comments">
                 { this.props.postComments.map(this.renderComment) }
-                <form ref="commentForm" className="comment-form">
+                <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit.bind(this)}>
                     <input type="text" ref="author" placeholder="author" />
                     <input type="text" ref="comment" placeholder="comment" />
                     <input type="submit" hidden />
