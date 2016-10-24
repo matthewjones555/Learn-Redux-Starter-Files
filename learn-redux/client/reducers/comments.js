674f3973ce5084ea5c5f9226b234f2823c17aa51
Matthew Jones <matthew.jones@slimmingworld.com>
2016-10-24 12:21:06 +0100

    Hooked up add and remove comments.  Used an example of reducer composition.

--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -2,9 +2,37 @@
  * Created by matthew.jones on 10/21/2016.
  */
 
+function postComments(state = [], action) {
+    switch (action.type) {
+        case 'ADD_COMMENT':
+            return [
+                ...state,
+                {
+                    user: action.author,
+                    text: action.comment,
+                },
+            ];
+        case 'REMOVE_COMMENT':
+            return [
+                ...state.slice(0, action.i),
+                ...state.slice(action.i + 1),
+            ];
+        default:
+            return state;
+    }
+}
+
 function comments(state = [], action) {
-    console.log(state, action);
-    return state;
+    switch (action.type) {
+        case 'ADD_COMMENT':
+        case 'REMOVE_COMMENT':
+            return {
+                ...state,
+                [action.postId]: postComments(state[action.postId], action),
+            };
+        default:
+            return state;
+    }
 }
 
 export default comments;
